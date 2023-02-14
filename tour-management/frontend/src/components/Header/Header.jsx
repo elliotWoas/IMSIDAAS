3f990eb1086cfdb13d6f65856eef587591ba8638
elliot sad <bamdadmahdi05@gmail.com>
2023-02-14 23:35:32 +0100

    start menu

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
 // we do not have a logo for shop {mehdi :D}
-import logo from '../../assets/images/logo.png';
+import logo from "../../assets/images/logo.png";
+import "./header.css";
 
 const nav__links = [
   {
@@ -12,31 +13,56 @@ const nav__links = [
   },
   {
     path: "",
-    display: "About"
+    display: "About",
   },
   {
     path: "/tours",
-    display: "Tours"
-  }
+    display: "Tours",
+  },
 ];
 
 const Header = () => {
-  return <header className="header">
-    <Container>
-      <Row>
-        <div className="nav__wrapper d-flex-align-items-center justify-content-between">
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex-align-items-center justify-content-between">
+            {/* ========= logo ========= */}
+            <div className="logo">
+              <img src="" alt="" />
+            </div>
+            {/* ========= logo end ========= */}
 
-         {/* ========= logo ========= */}
-         <div className="logo">
-          <img src="" alt="" />
-         </div>
-         {/* ========= logo end========= */}
+            {/* ========= menu start ========= */}
+            <div className="navigation">
+              <ul className="menu d-flex align-item-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item">
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* ========= menu end========= */}
 
-          
-        </div>
-      </Row>
-    </Container>
-  </header>;
-}
+            <div className="nav__right d-flex align-item-center gap-4">
+              <div className="nav__btns d-flex align-item-center gap-4">
+                <button className="btn secondary__btn ">
+                  <link to="/login">Login</link>
+                </button>
+                <button className="btn priamry__btn ">
+                  <link to="/register">Register</link>
+                </button>
+              </div>
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
 
 export default Header;
