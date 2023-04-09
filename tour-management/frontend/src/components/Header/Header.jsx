2a2d1cdf7be0e0f0171be6f15ea81e1a0e5fbe95
Arshiya <arshiyah52@gmail.com>
2023-04-10 03:41:36 +0430

    fix bug by me :)

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Container, Row, Button } from "reactstrap";
-import { NavLink, Link, UNSAFE_NavigationContext } from "react-router-dom";
+import { Container, Row } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
 
 // we do not have a logo for shop {mehdi :D}
-import logo from "../../assets/images/logo.png";
+// import logo from "../../assets/images/logo.png";
 import "./header.css";
 
 const nav__links = [
@@ -29,7 +29,7 @@ const Header = () => {
           <div className="nav__wrapper d-flex align-items-center justify-content-between">
             {/* ========= logo ========= */}
             <div className="logo">
-              <img src={logo} alt="" />
+              {/*<img src="" alt="" />*/}
             </div>
             {/* ========= logo end========= */}
 
@@ -40,9 +40,10 @@ const Header = () => {
                   <li className="nav__item" key={index}>
                     <NavLink
                       to={item.path}
-                      className={(navClass) =>
-                        navClass.isActive ? "active__link" : ""
-                      }
+                      // className={(navClass) =>
+                      //     navClass.isActive ? "active__link" : ""
+                      // }
+                      className=""
                     >
                       {item.display}
                     </NavLink>
@@ -55,14 +56,14 @@ const Header = () => {
             <div className="nav__right d-flex align-item-center gap-4">
               <div className="nav__btns d-flex align-item-center gap-4">
                 <button className="btn secondary__btn ">
-                  <link to="/login">Login</link>
+                  <Link to="/login">Login</Link>
                 </button>
                 <button className="btn priamry__btn ">
-                  <link to="/register">Register</link>
+                  <Link to="/register">Register</Link>
                 </button>
               </div>
               <span className="mobile__menu">
-                <i class="ri-menu-line"></i>
+                <i className="ri-menu-line"></i>
               </span>
             </div>
           </div>
