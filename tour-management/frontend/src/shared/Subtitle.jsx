d79740de09c1adba4f82258be7d1efe3fae85628
elliot sad <bamdadmahdi05@gmail.com>
2023-04-15 23:32:37 +0200

    subtitle better now

--- tour-management/frontend/src/shared/Subtitle.jsx
+++ tour-management/frontend/src/shared/Subtitle.jsx
@@ -1,11 +1,7 @@
-
-
 import React from "react";
 
-const Subtitle = ({subtitle}) => {
-    return (
-        <h3 className='section__subtitle'>{subtitle}</h3>
-    )
-}
+const Subtitle = ({ subtitle }) => {
+  return <h3 className="section__subtitle">{subtitle}</h3>;
+};
 
-export default Subtitle
+export default Subtitle;
