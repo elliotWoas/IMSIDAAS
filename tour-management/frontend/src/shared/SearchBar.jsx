f41a545aac8f9ff33813f64e0d36f2729cd1b86a
elliot sad <bamdadmahdi05@gmail.com>
2023-04-18 02:15:59 +0200

    finish searchbar : )

---
+++ tour-management/frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./search-bar.css";
+import { Col, Form, FormGroup } from "reactstrap";
+
+//search bar component
+const SearchBar = () => {
+  return <Col lg='12'>
+    <div className="search__bar">
+      <Form className="d-flex align-items-center gap-4">
+        <FormGroup className="d-flex gap-3 form__group form__group-fast">
+          <span>
+            <i class="ri-map-pin-line"></i>
+          </span>
+          <div>
+            <h6>Location</h6>
+            <input type="text" placeholder="Where are you going?" />
+          </div>
+        </FormGroup>
+        <FormGroup className="d-flex gap-3 form__group form__group-fast">
+          <span>
+            <i class="ri-map-pin-time-line"></i>
+          </span>
+          <div>
+            <h6>Distance</h6>
+            <input type="number" placeholder="Distance k/m" />
+          </div>
+        </FormGroup>
+        <FormGroup className="d-flex gap-3 form__group form__group-last">
+          <span>
+            <i class="ri-group-line"></i>
+          </span>
+          <div>
+            <h6>Max People</h6>
+            <input type="number" placeholder="0" />
+          </div>
+        </FormGroup>
+
+        <span className="search__icon" type='submit'>
+            <i class="ri-search-line"></i>
+        </span>
+      </Form>
+    </div>
+  </Col>;
+};
+
+export default SearchBar;
