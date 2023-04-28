b3e492b4386087c1ab6eef908169c326f8bf224d
elliot sad <bamdadmahdi05@gmail.com>
2023-04-28 22:22:35 +0200

    start service list

---
+++ tour-management/frontend/src/services/ServiceList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ServiceCard from "./ServiceCard";
+import { Col } from "reactstrap";
+
+import weatherImg from "../assets/images/weather.png";
+import guideImg from "../assets/images/guide.png";
+import customizationImg from "../assets/images/customization.png";
+
+const servicesData = [
+  {
+    imgUrl: weatherImg,
+    title: "Calculate weather ",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    imgUrl: guideImg,
+    title: "Best Tour Guide",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    imgUrl: customizationImg,
+    title: "customizationImg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+];
+
+import React from "react";
+
+const ServiceList = () => {
+  return <>
+  </>;
+};
+
+export default ServiceList;
