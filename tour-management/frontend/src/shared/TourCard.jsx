b1428a05f00f69b8ea157ee6862474475537be37
elliot sad <bamdadmahdi05@gmail.com>
2023-05-07 00:12:31 +0200

    fix bug tourcard home page

--- tour-management/frontend/src/shared/TourCard.jsx
+++ tour-management/frontend/src/shared/TourCard.jsx
@@ -16,34 +16,35 @@ const TourCard = ({ tour }) => {
           <img src={photo} alt="tour-img" />
           <span>Featured</span>
         </div>
-      </Card>
-      <CardBody>
-        <div
-          className="card__top d-flex align-items-center 
+        <CardBody>
+          <div
+            className="card__top d-flex align-items-center 
         justify-content-between"
-        >
-          <span className="tour__location d-flex align-items-center gap-1">
-            <i class="ri-map-pin-line"></i> {city}
-          </span>{" "}
-          <span className="tour__rating d-flex align-items-center gap-1">
-            <i class="ri-star-fill"></i> {avgRating}{" "}
-            <span>({reviews.length})</span>
-          </span>
-        </div>
-         {/* name title location  in the box */}
-        <h6 className="tour__title">
-          <Link to={`/tour/${id}`}>{title}</Link>
-        </h6>
-        <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
-          <h6>
-            ${price} <span>/per person</span>
+          >
+            <span className="tour__location d-flex align-items-center gap-1">
+              <i class="ri-map-pin-line"></i> {city}
+            </span>{" "}
+            <span className="tour__rating d-flex align-items-center gap-1">
+              <i class="ri-star-fill"></i> {avgRating}{" "}
+              <span>({reviews.length})</span>
+            </span>
+          </div>
+          {/* name title location  in the box */}
+          <h6 className="tour__title">
+            <Link to={`/tour/${id}`}>{title}</Link>
           </h6>
-
-          <button className="btn booking__btn">
-            <Link to={`/tour/${id}`}><h6>Book Now</h6></Link>
-          </button>
-        </div>
-      </CardBody>
+          <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
+            <h6>
+              ${price} <span>/per person</span>
+            </h6>
+            <button className="btn booking__btn">
+              <Link to={`/tour/${id}`}>
+                <h6>Book Now</h6>
+              </Link>
+            </button>
+          </div>
+        </CardBody>
+      </Card>
     </div>
   );
 };
