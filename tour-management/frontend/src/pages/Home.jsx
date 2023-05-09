ae0a62ff3c78a13968d8d560ead37377ca4c4d59
elliot sad <bamdadmahdi05@gmail.com>
2023-05-09 21:55:57 +0200

    experience section start: 01 home page

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -11,81 +11,120 @@ import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 
-
 const Home = () => {
   return (
     <>
-
-                {/*  hero section start */}
+      {/*  hero section start */}
       <section>
         <Container>
           <Row>
             <Col lg="6">
-            <div className="hero__content">
-              <div className="hero__subtitle d-flex align-items-center ">
-                <Subtitle subtitle={'Know Before You Go'} />
-                <img src={worldImg} alt="" />
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center ">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight"> memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Minima nobis pariatur ea laboriosam sapiente dolor, modi
+                  aperiam doloremque voluptatem a beatae earum
+                </p>
               </div>
-              <h1>
-                Traveling opens the door to creating{" "}
-                <span className="highlight"> memories</span>
-              </h1>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-                nobis pariatur ea laboriosam sapiente dolor, modi aperiam
-                doloremque voluptatem a beatae earum
-              </p>
-            </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box">
                 <img src={heroImg} alt="" />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-4">
                 <video src={heroVideo} alt="" controls />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-5">
-                <img src={heroImg02} alt=""  />
+                <img src={heroImg02} alt="" />
               </div>
             </Col>
             <SearchBar />
           </Row>
         </Container>
       </section>
-                        {/* hero section start */}
+      {/* hero section start */}
 
-          <section>
-            <Container>
-              <Row>
-                <Col lg='3'>
-                  <h5 className="services__subtitle">What we serve</h5>
-                  <h2 className="services__title">We offer our best services</h2>
-                </Col>
-                <ServiceList/>
-              </Row>
-            </Container>
-          </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
 
-          {/* ======= featured tour section start ======== */}
-           
-           <section>
-            <Container>
-              <Row>
-                <Col lg="12" className="mb-5">
-                  <Subtitle subtitle={'Explore'} />
-                  <h2 className="featured__tour-title">Our featured tours</h2>
-                </Col>
-                <FeaturedTourList/>
-              </Row>
-            </Container>
-           </section>
+      {/* ======= featured tour section start ======== */}
 
-          {/* ======= featured tour section end ======== */}  
-    </>              
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+
+      {/* ========= featured tour section end ========== */}
+
+      {/* ==========  experience section start ==========*/}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="mb-">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+
+                <h2>
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  lorem ipsuam dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                  Quas aliquam, hic tempora inventor suscipit unde.
+                </p>
+              </div>
+              <div className="counter__wrapper d-flex align-center gap-5">
+
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Regular clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6"></Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* ==========  experience section end ==========*/}
+    </>
   );
 };
 
