365e976bf663dba959c668e769c032ab30459b62
elliot sad <bamdadmahdi05@gmail.com>
2023-05-12 23:57:32 +0200

    zoom in gallery image

--- tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
+++ tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -4,14 +4,15 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const MasonryImagesGallery = () => {
   return (
-    <ResponsiveMasonry columnsCountBreakPoints={{350:1, 768:3, 992:4}}>
+    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 768: 3, 992: 4 }}>
       <Masonry gutter="1rem">
         {galleryImages.map((item, index) => (
           <img
+            className="masonry__img"
             src={item}
             key={index}
             alt=""
-            style={{ width: "100%", display: "block", borderRadius: "10px"}}
+            style={{ width: "100%", display: "block", borderRadius: "10px" }}
           />
         ))}
       </Masonry>
