843a24f808a7cd4abda7b81ca98eb41768cba46a
elliot sad <bamdadmahdi05@gmail.com>
2023-05-14 22:15:50 +0200

    footer

--- tour-management/frontend/src/components/Footer/Footer.jsx
+++ tour-management/frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+
+const quick__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
+const quick__links2 = [
+  {
+    path: "/gallery",
+    display: "Gallery",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+  {
+    path: "/register",
+    display: "Register",
+  },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="" />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Suscipit excepturi doloribus cum itaque voluptas quidem nemo a
+                libero deleniti tempora veritatis necessitatibus temporibus
+              </p>
 
-export default Footer
+              <div className="social__links  d-flex align-items-center gap-4">
+                <span>
+                  <Link to="#">
+                    <i class="ri-youtube-line"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-github-fill"></i>
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-facebook-circle-line"></i>
+                  </Link>
+                </span>{" "}
+                <span>
+                  <Link to="#">
+                    <i class="ri-instagram-line"></i>
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
