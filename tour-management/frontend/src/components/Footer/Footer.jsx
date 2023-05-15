f5cfd7e614bcf8b7d08ebe7635d676866f02a399
elliot sad <bamdadmahdi05@gmail.com>
2023-05-16 00:39:23 +0200

    completed footer all page

--- tour-management/frontend/src/components/Footer/Footer.jsx
+++ tour-management/frontend/src/components/Footer/Footer.jsx
@@ -37,6 +37,8 @@ const quick__links2 = [
 ];
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <Container>
@@ -44,11 +46,7 @@ const Footer = () => {
           <Col lg="3">
             <div className="logo">
               <img src={logo} alt="" />
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Suscipit excepturi doloribus cum itaque voluptas quidem nemo a
-                libero deleniti tempora veritatis necessitatibus temporibus
-              </p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
 
               <div className="social__links  d-flex align-items-center gap-4">
                 <span>
@@ -73,6 +71,64 @@ const Footer = () => {
                 </span>
               </div>
             </div>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__linke-title">Discover</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__linke-title">Quick Links</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__linke-title">Content</h5>
+            <ListGroup className="footer__quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  Address:
+                </h6>
+                <p className="mb-0">Ardbil, iran</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  Email:
+                </h6>
+                <p className="mb-0">mahdi1382bbamdad@gmail.com</p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-fill"></i>
+                  </span>
+                  Phone:
+                </h6>
+                <p className="mb-0">+9928432808</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="12" className="text-center pt-5">
+            <p className="copyright">
+              {year}, design and develop by mehdi omidvar. All rights
+              reserved
+            </p>
           </Col>
         </Row>
       </Container>
