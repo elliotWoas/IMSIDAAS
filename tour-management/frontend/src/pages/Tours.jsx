9322a1b3a0da8b7280ae0b757c6ee29c9558adb8
elliot sad <bamdadmahdi05@gmail.com>
2023-05-18 03:46:52 +0200

    tours: add searchbar, newsletter, tourcard...

--- tour-management/frontend/src/pages/Tours.jsx
+++ tour-management/frontend/src/pages/Tours.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import CommonSection from "../shared/CommonSection";
+
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+import TourCard from "./../shared/TourCard";
+import SearchBar from "./../shared/SearchBar";
+import Newsletter from "./../shared/Newsletter";
+
+import { Container, Row, Col } from "reactstrap";
 
 const Tours = () => {
   return (
-    <div>Tours</div>
-  )
-}
+    <>
+      <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Tours
+export default Tours;
