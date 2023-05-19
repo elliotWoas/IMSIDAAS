e7e8d38a956122b6c5679bfc24b16f9f89f97bce
elliot sad <bamdadmahdi05@gmail.com>
2023-05-19 21:58:45 +0200

    tours : add page count for featured

--- tour-management/frontend/src/pages/Tours.jsx
+++ tour-management/frontend/src/pages/Tours.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
 
 import "../styles/tour.css";
@@ -10,6 +10,14 @@ import Newsletter from "./../shared/Newsletter";
 import { Container, Row, Col } from "reactstrap";
 
 const Tours = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5 / 4);
+    setPageCount(pages);
+  }, [page]);
+
   return (
     <>
       <CommonSection title={"All Tours"} />
@@ -24,13 +32,26 @@ const Tours = () => {
         <Container>
           <Row>
             {tourData?.map((tour) => (
-              <Col lg="3" key={tour.id}>
+              <Col lg="3" className="mb-4" key={tour.id}>
                 <TourCard tour={tour} />
               </Col>
             ))}
+            <Col lg="12">
+              <div
+                className="pagination d-flex align-items-center
+               justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map(number => (
+                  <span key={number} onClick={()=> setPage(number)}
+                  className={ page===number ? 'active__page' : ''}>
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
+      
     </>
   );
 };
