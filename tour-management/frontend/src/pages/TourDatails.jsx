8a61a85e170601b0a3b6ae68c6562744d5c7182d
elliot sad <bamdadmahdi05@gmail.com>
2023-05-20 05:51:52 +0200

    tourDetails : fix err  router

--- tour-management/frontend/src/pages/TourDatails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TourDatails = () => {
-  return (
-    <div> TourDatails</div>
-  )
-}
-
-export default TourDatails
--- tour-management/frontend/src/router/Routers.jsx
+++ tour-management/frontend/src/router/Routers.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate, } from "react-router-dom";
 
 import Home from "./../pages/Home";
 import Tours from "./../pages/Tours";
-import TourDatails from "./../pages/TourDatails";
+import TourDetails from "./../pages/TourDetails";
 import Login from "./../pages/Login";
 import Register from "./../pages/Register";
 import SearchResultList from "./../pages/SearchResultList";
@@ -14,7 +14,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/tours" element={<Tours />} />
-      <Route path="/tours/:id" element={<TourDatails />} />
+      <Route path="/tours/:id" element={<TourDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/tours/search" element={<SearchResultList />} />
