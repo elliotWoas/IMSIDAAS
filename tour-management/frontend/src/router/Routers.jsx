605d2b8515cf4f56ffa53fd961fb8fbc0b4384f8
elliot sad <bamdadmahdi05@gmail.com>
2023-05-23 22:09:09 +0200

    routs: fixed err not match routs id in tourDetails

--- tour-management/frontend/src/router/Routers.jsx
+++ tour-management/frontend/src/router/Routers.jsx
@@ -14,7 +14,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/tours" element={<Tours />} />
-      <Route path="/tours/:id" element={<TourDetails />} />
+      <Route path="/tour/:id" element={<TourDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/tours/search" element={<SearchResultList />} />
