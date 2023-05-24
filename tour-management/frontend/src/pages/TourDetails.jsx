9374913baf4395d365385edcfed184adc5103aaa
elliot sad <bamdadmahdi05@gmail.com>
2023-05-24 21:59:06 +0200

    tourDetails : tour content

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -3,17 +3,28 @@ import "../styles/tour-details.css";
 import { Container, Row, Col, From, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
 import tourData from "../assets/data/tours";
+import calculateAvgRating from "../utils/avgRating";
 
 const TourDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
 
   //this is an static data later we will call our API and load our data from database
   const tour = tourData.find((tour) => tour.id === id);
 
   // destructure properties from tour object
-  const { photo, title, desc, price, reviews, city, distance, maxGroupSize } =
-    tour;
+  const {
+    photo,
+    title,
+    desc,
+    price,
+    address,
+    reviews,
+    city,
+    distance,
+    maxGroupSize,
+  } = tour;
 
+  const { totalRating, avgRating } = calculateAvgRating(reviews);
   return (
     <>
       <section>
@@ -25,6 +36,37 @@ const TourDetails = () => {
 
                 <div className="tour__info">
                   <h2>{title}</h2>
+                  <div className="d-flex align-items-center gap-5">
+                    <span className="tour__rating d-flex align-items-center gap-1">
+                      <i
+                        class="ri-star-s-fill"
+                        style={{ color: "var(--secondary-color)" }}
+                      ></i>{" "}
+                      {calculateAvgRating === 0 ? null : avgRating}
+                      {totalRating === 0 ? (
+                        "Not rated"
+                      ) : (
+                        <span>({reviews?.length})</span>
+                      )}
+                    </span>
+
+                    <span>
+                      <i class="ri-map-pin-user-fill"></i> {address}
+                    </span>
+                  </div>
+
+                  <div className="tour__extra-details">
+                    <span>
+                      <i class="ri-map-pin-2-line"></i> {city}{" "}
+                    </span>
+                    <span>
+                      <i class="ri-money-dollar-circle-line"></i> ${price} /per
+                      person
+                    </span>
+                    <span>
+                      <i class="ri-group-line"></i> {maxGroupSize}{" "}
+                    </span>
+                  </div>
                 </div>
               </div>
             </Col>
