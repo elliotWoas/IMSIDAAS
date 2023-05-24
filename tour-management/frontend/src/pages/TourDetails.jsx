7d4e35bc52084d8c6f403cf6ed9b9baf2c876068
elliot sad <bamdadmahdi05@gmail.com>
2023-05-25 00:04:29 +0200

    tourDetails : tour reviws section

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../styles/tour-details.css";
-import { Container, Row, Col, From, ListGroup } from "reactstrap";
+import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
 import tourData from "../assets/data/tours";
 import calculateAvgRating from "../utils/avgRating";
+import avatar from "../assets/images/avatar.jpg";
 
 const TourDetails = () => {
   const { id } = useParams();
+  const reviewMsgRef = useRef("");
+  const [tourRating, setTourRating] = useState(null);
 
   //this is an static data later we will call our API and load our data from database
   const tour = tourData.find((tour) => tour.id === id);
@@ -25,6 +28,18 @@ const TourDetails = () => {
   } = tour;
 
   const { totalRating, avgRating } = calculateAvgRating(reviews);
+
+  // format date
+  const options = { day: "numeric", month: "long", year: "numeric" };
+
+  // submit request to the server
+  const submitHandler = e => {
+    e.preventDefault()
+    const reviewText = reviewMsgRef.current.value
+
+    alert(`${reviewText}, ${tourRating}`)
+    // later will call our API
+  }
   return (
     <>
       <section>
@@ -64,12 +79,81 @@ const TourDetails = () => {
                       person
                     </span>
                     <span>
-                      <i class="ri-group-line"></i> {maxGroupSize}{" "}
+                      <i class="ri-map-pin-time-line"></i> {distance} k/m
+                    </span>
+                    <span>
+                      <i class="ri-group-line"></i> {maxGroupSize} people{" "}
                     </span>
                   </div>
                   <h5>Description</h5>
                   <p>{desc}</p>
                 </div>
+                {/* ========== tour reviws section ========== */}
+                <div className="tour__reviws mt-4">
+                  <h4>Reviws ({reviews?.length} reviws)</h4>
+
+                  <Form onSubmit={submitHandler}>
+                    <div className="d-flex align-items-center gap-3 mb-4 rating__group">
+                      <span onClick={() => setTourRating(1)}>
+                        1 <i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(2)}>
+                        2 <i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(3)}>
+                        3 <i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(4)}>
+                        4 <i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(5)}>
+                        5 <i class="ri-star-s-fill"></i>
+                      </span>
+                    </div>
+                    <div className="review__input">
+                      <input
+                        type="text"
+                        ref={reviewMsgRef}
+                        placeholder="share your thoughts"
+                        required
+                      />
+                      <button
+                        className="btn primary__btn text-white"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </Form>
+
+                  <ListGroup className="user__reviews">
+                    {reviews?.map((review) => (
+                      <div className="review__item">
+                        <img src={avatar} alt="" />
+
+                        <div className="w-100">
+                          <div className="d-flex align-items-center justify-content-between">
+                            <div>
+                              <h5>mehdi</h5>
+                              <p>
+                                {new Date("05-18-2023").toLocaleDateString(
+                                  "en-US",
+                                  options
+                                )}
+                              </p>
+                            </div>
+                            <span className="d-flex align-items-center">
+                              5 <i class="ri-star-s-fill"></i>
+                            </span>
+                          </div>
+
+                          <h6>Amazing tour</h6>
+                        </div>
+                      </div>
+                    ))}
+                  </ListGroup>
+                </div>
+                {/* ========== tour reviws section end ========== */}
               </div>
             </Col>
           </Row>
