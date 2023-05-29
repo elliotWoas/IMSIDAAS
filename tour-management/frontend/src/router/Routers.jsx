de6c3ff40f0a8cb681cb9553fa8c0a240fc943bb
elliot sad <bamdadmahdi05@gmail.com>
2023-05-29 22:58:12 +0200

    add routs thank you page

--- tour-management/frontend/src/router/Routers.jsx
+++ tour-management/frontend/src/router/Routers.jsx
@@ -7,6 +7,7 @@ import TourDetails from "./../pages/TourDetails";
 import Login from "./../pages/Login";
 import Register from "./../pages/Register";
 import SearchResultList from "./../pages/SearchResultList";
+import ThankYou from "../pages/ThankYou";
 
 const Routers = () => {
   return (
@@ -17,6 +18,7 @@ const Routers = () => {
       <Route path="/tour/:id" element={<TourDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/thank-you" element={<ThankYou />} />
       <Route path="/tours/search" element={<SearchResultList />} />
     </Routes>
   );
