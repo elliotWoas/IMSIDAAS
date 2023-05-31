51d8ea8485dd3fce500c52b1715652cd32aad44c
elliot sad <bamdadmahdi05@gmail.com>
2023-05-31 22:06:33 +0200

    login : front login

--- tour-management/frontend/src/pages/Login.jsx
+++ tour-management/frontend/src/pages/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+
+import loginImg from "../assets/images/login.png";
+import useIcon from "../assets/images/user.png";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined,
+  });
 
-export default Login
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="15">
+            <div className="login__container d-flex align-items-center justify-content-between">
+              <div className="login__img">
+                <img src={loginImg} alt="" />
+              </div>
+
+              <div className="login__form">
+                <div className="user">
+                  <img src={useIcon} alt="" />
+                </div>
+                <h2>Login</h2>
+
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <Button
+                    className="btn secondary__btn auth__btn "
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form>
+                <p>
+                  Don't have an account? <Link to="/register">Create</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Login;
