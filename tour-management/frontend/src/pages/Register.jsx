5230a8da5d3ebf4b69ccb0c3bc76cbec6f086d96
elliot sad <bamdadmahdi05@gmail.com>
2023-05-31 22:06:49 +0200

    register : front register

--- tour-management/frontend/src/pages/Register.jsx
+++ tour-management/frontend/src/pages/Register.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
+
+import registerImg from "../assets/images/register.png";
+import useIcon from "../assets/images/user.png";
 
 const Register = () => {
-    return (
-        <div>Register</div>
-    )
-}
+  const [credentials, setCredentials] = useState({
+    userName: undefined,
+    email: undefined,
+    password: undefined
+  });
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="15">
+            <div className="login__container d-flex align-items-center justify-content-between">
+              <div className="login__img">
+                <img src={registerImg} alt="" />
+              </div>
+
+              <div className="login__form">
+                <div className="user">
+                  <img src={useIcon} alt="" />
+                </div>
+                <h2>Register</h2>
+
+                <Form onSubmit={handleClick}>
+                <FormGroup>
+                    <input
+                      type="text"
+                      placeholder="Username"
+                      required
+                      id="username"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <Button
+                    className="btn secondary__btn auth__btn "
+                    type="submit"
+                  >
+                    Create Account
+                  </Button>
+                </Form>
+                <p>
+                  Already have an account? <Link to="/login">Login</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
 
 export default Register
