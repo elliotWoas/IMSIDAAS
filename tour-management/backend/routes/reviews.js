7d927b5cb1ac42391edc4f323e37d64df0277148
elliot sad <bamdadmahdi05@gmail.com>
2023-06-28 22:34:17 +0200

    routs reviews : add verifyuser to middleware route

--- tour-management/backend/routes/reviews.js
+++ tour-management/backend/routes/reviews.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createReview } from "../controllers/reviewController.js";
+import { verifyUser} from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/:tourId", createReview);
+router.post("/:tourId", verifyUser, createReview);
 
 export default router;
