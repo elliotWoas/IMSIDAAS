7b5e3040676de1659e80ccaa8983acb3ab44736f
elliot sad <bamdadmahdi05@gmail.com>
2023-07-06 22:19:26 +0200

    routes booking : fix err verifyuser

--- tour-management/backend/routes/bookings.js
+++ tour-management/backend/routes/bookings.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyUser} from "../utils/verifyUser.js";
+import { verifyUser} from "../utils/verifyToken.js";
 import { createBooking } from "../controllers/bookingController.js";
 
 const router = express.Router();
