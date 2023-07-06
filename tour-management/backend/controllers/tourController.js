719147a04b9297808cfb1de8dc698b77f66e9509
elliot sad <bamdadmahdi05@gmail.com>
2023-07-06 22:18:39 +0200

    tour controller : add populate to get single tour

--- tour-management/backend/controllers/tourController.js
+++ tour-management/backend/controllers/tourController.js
@@ -64,7 +64,7 @@ export const deleteTour = async (req, res) => {
 export const getSingleTour = async (req, res) => {
   const id = req.params.id;
   try {
-    const tour = await Tour.findById(id);
+    const tour = await Tour.findById(id).populate("reviews");
 
     res.status(200).json({
       success: true,
