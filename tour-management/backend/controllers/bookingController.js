24dd9def29be02a654ddc247f35193c836d101aa
elliot sad <bamdadmahdi05@gmail.com>
2023-07-10 22:33:08 +0200

    booking controller : create new booking and save

--- tour-management/backend/controllers/bookingController.js
+++ tour-management/backend/controllers/bookingController.js
@@ -1,5 +1,17 @@
-import booking from '../models/Booking.js'
+import Booking from "../models/Booking.js";
 export const createBooking = async (req, res) => {
+  const newBooking = new Booking(req.body);
   try {
-  } catch (err) {}
+    const savedBooking = await newBooking.save();
+    res.status(200).json({
+      success: true,
+      message: "You tour is booked",
+      data: savedBooking,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "internal server error",
+    });
+  }
 };
