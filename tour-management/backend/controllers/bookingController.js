d1dcf025c7bdf88182356ec19585e182928fda86
elliot sad <bamdadmahdi05@gmail.com>
2023-07-11 22:08:21 +0200

    booking controller : get booking

--- tour-management/backend/controllers/bookingController.js
+++ tour-management/backend/controllers/bookingController.js
@@ -1,4 +1,6 @@
 import Booking from "../models/Booking.js";
+
+// create new booking
 export const createBooking = async (req, res) => {
   const newBooking = new Booking(req.body);
   try {
@@ -13,5 +15,17 @@ export const createBooking = async (req, res) => {
       success: false,
       message: "internal server error",
     });
+  }
+};
+
+//get single booking
+export const getBooking = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const book = await Booking.findById(id);
+
+    res.status(200).json({ success: true, message: "successful", data: book });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "not found" });
   }
 };
