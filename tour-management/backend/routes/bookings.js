82c8bff678ead8ebafed86ed80eaba6ae9323b79
elliot sad <bamdadmahdi05@gmail.com>
2023-07-12 22:19:45 +0200

    routes booking : add verify admin and get, all

--- tour-management/backend/routes/bookings.js
+++ tour-management/backend/routes/bookings.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { verifyUser} from "../utils/verifyToken.js";
-import { createBooking } from "../controllers/bookingController.js";
+import {
+  createBooking,
+  getAllBooking,
+  getBooking,
+} from "../controllers/bookingController.js";
+
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/", verifyUser, createBooking);
+router.get("/:id", verifyUser, getBooking);
+router.post("/", verifyAdmin, getAllBooking);
 
 export default router;
