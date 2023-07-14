bc98dc6fc7f0f6415006ba011632b67ea80cd62e
elliot sad <bamdadmahdi05@gmail.com>
2023-07-14 22:28:20 +0200

    frontend hooks :  start useFetch

---
+++ tour-management/frontend/src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+      } catch (err) {}
+    };
+  },[]);
+
+  return;
+};
+
+export default useFetch;
