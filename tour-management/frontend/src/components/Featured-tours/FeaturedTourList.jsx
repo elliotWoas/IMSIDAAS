dc6c3277e0697d661b84e0c5d99d3735f7491985
elliot sad <bamdadmahdi05@gmail.com>
2023-07-19 22:23:15 +0200

    baseurl log

--- tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -4,12 +4,18 @@ import TourData from "../../assets/data/tours";
 import { Col } from "reactstrap";
 
 import useFetch from "../../hooks/useFetch";
-import { BASE_URL  } from "../../utils/config";
+import { BASE_URL } from "../../utils/config";
 
 const FeaturedTourList = () => {
+  const { data: featuredTours } = useFetch(
+    `${BASE_URL}tours/search/getFeaturedTours`
+  );
+
+  console.log(featuredTours);
+
   return (
     <>
-      {TourData?.map(tour => (
+      {TourData?.map((tour) => (
         <Col lg="3" className="mb-4" key={tour.id}>
           <TourCard tour={tour} />
         </Col>
