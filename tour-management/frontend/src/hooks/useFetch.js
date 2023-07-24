42604e967102c41de8b67b94abcca064a5246934
elliot sad <bamdadmahdi05@gmail.com>
2023-07-24 22:20:47 +0200

    use fetch : set loading false

--- tour-management/frontend/src/hooks/useFetch.js
+++ tour-management/frontend/src/hooks/useFetch.js
@@ -15,7 +15,9 @@ const useFetch = (url) => {
           setError("failed to fetch");
         }
         const result = await res.json();
+
         setData(result.data);
+        setLoading(false);
       } catch (err) {
         setError(err.message);
         setLoading(false);
