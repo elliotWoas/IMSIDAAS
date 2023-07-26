2964c9e71b96e74fc619fa96d5956344a5656d40
elliot sad <bamdadmahdi05@gmail.com>
2023-07-26 22:22:43 +0200

    search bar : fetch navigate bar

--- tour-management/frontend/src/shared/SearchBar.jsx
+++ tour-management/frontend/src/shared/SearchBar.jsx
@@ -2,13 +2,17 @@ import React, { useRef } from "react";
 import "./search-bar.css";
 import { Col, Form, FormGroup } from "reactstrap";
 
+import { BASE_URL } from "../utils/config";
+
+import { useNavigate } from "react-router-dom";
+
 //search bar component and validation
 const SearchBar = () => {
   const locationRef = useRef("");
   const distanceRef = useRef(0);
   const maxGroupSizeRef = useRef(0);
-
-  const searchHandler = () => {
+  const navigate = useNavigate();
+  const searchHandler = async () => {
     const location = locationRef.current.value;
     const distance = distanceRef.current.value;
     const maxGroupSize = maxGroupSizeRef.current.value;
@@ -16,6 +20,18 @@ const SearchBar = () => {
     if (location === "" || distance === "" || maxGroupSize === "") {
       return alert("All fields are required!");
     }
+    const res = await fetch(`${BASE_URL}/tours/search/getTourBySearch?city=
+    ${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`);
+
+    if (!res.ok) alert("Something went wrong!");
+
+    const result = await res.json();
+
+    navigate(
+      `/tours/search?city=${location}&distance=${distance}&maxGroupSize=
+      ${maxGroupSize}`,
+      { state: result.data }
+    );
   };
 
   return (
