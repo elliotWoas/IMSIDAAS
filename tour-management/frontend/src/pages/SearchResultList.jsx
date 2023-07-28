1dfc4a8be102353f6e2fb2866bcf7f7a5b9d9823
elliot sad <bamdadmahdi05@gmail.com>
2023-07-28 22:22:14 +0200

    search result list : add use state

--- tour-management/frontend/src/pages/SearchResultList.jsx
+++ tour-management/frontend/src/pages/SearchResultList.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 import CommonSection from "./../shared/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+
+import { useLocation } from "react-router-dom";
+import TourCard from "./../shared/TourCard";
+
 const SearchResultList = () => {
-  return <>
-  <CommonSection title={'Tour Search Result'} />
-  </>
+
+  const location = useLocation();
+
+  const [data] = useState(location.state);
+
+  console.log(data);
+  return (
+    <>
+      <CommonSection title={"Tour Search Result"} />
+      <section>
+        <Container>
+          <Row>
+
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default SearchResultList;
