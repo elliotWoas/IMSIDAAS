8d40815fa2ab997b0fb62191a50c4c14b24c26e8
elliot sad <bamdadmahdi05@gmail.com>
2023-07-29 22:30:27 +0200

    search result list : find tour search bar

--- tour-management/frontend/src/pages/SearchResultList.jsx
+++ tour-management/frontend/src/pages/SearchResultList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import CommonSection from "./../shared/CommonSection";
 import { Container, Row, Col } from "reactstrap";
@@ -7,7 +7,6 @@ import { useLocation } from "react-router-dom";
 import TourCard from "./../shared/TourCard";
 
 const SearchResultList = () => {
-
   const location = useLocation();
 
   const [data] = useState(location.state);
@@ -19,7 +18,15 @@ const SearchResultList = () => {
       <section>
         <Container>
           <Row>
-
+            {data.length === 0 ? (
+              <h4 className="text-center">No tour found</h4>
+            ) : (
+              data?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))
+            )}
           </Row>
         </Container>
       </section>
