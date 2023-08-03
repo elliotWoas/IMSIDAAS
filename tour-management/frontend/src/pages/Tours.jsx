2bfcdbeac554e4f3bf0f5a70e592975b2fce5c1e
elliot sad <bamdadmahdi05@gmail.com>
2023-08-03 23:16:26 +0200

    tours:tours loading & error & base url & tourcount

--- tour-management/frontend/src/pages/Tours.jsx
+++ tour-management/frontend/src/pages/Tours.jsx
@@ -9,14 +9,20 @@ import Newsletter from "./../shared/Newsletter";
 
 import { Container, Row, Col } from "reactstrap";
 
+import useFetch from '../hooks/useFetch'
+import { BASE_URL } from "../utils/config";
+
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
+  const { data:tours , loading, error } = useFetch(`${BASE_URL}/tours`)
+  const { data:tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`) 
+
   useEffect(() => {
     const pages = Math.ceil(5 / 4);
     setPageCount(pages);
-  }, [page]);
+  }, [page, tourCount]);
 
   return (
     <>
@@ -32,8 +38,8 @@ const Tours = () => {
         <Container>
           {!loading && !error && (
             <Row>
-              {tourData?.map((tour) => (
-                <Col lg="3" className="mb-4" key={tour.id}>
+              {tours?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
                   <TourCard tour={tour} />
                 </Col>
               ))}
