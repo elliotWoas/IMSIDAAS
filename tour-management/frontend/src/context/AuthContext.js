d8044f9df05e89e2e2fa9dc15b5e9f65d75cd300
elliot sad <bamdadmahdi05@gmail.com>
2023-08-12 23:45:35 +0200

    auth context : auth reducer

---
+++ tour-management/frontend/src/context/AuthContext.js
@@ -0,0 +1,47 @@
+import { createContext, useEffect, useReducer } from "react"
+
+const initial_state = {
+    user: null,
+    loading: false,
+    error: null,
+};
+
+export const AuthContext = createContext(initial_state);
+
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN_START": 
+        return {
+            user: null,
+            loading: false,
+            error: null,
+        };
+        case 'LOGIN_SUCCESS' :
+            return {
+                user: action.playload,
+                loading: false,
+                error: null,
+            } 
+            case 'LOGIN_FAILURE' :
+                return {
+                    user: null,
+                    loading: false,
+                    error: action.playload,
+                } 
+                case 'REGISTER_SUCCESS' :
+                    return {
+                        user: null,
+                        loading: false,
+                        error: null,
+                    }
+                    case 'LOGOUT' :
+                    return {
+                        user: null,
+                        loading: false,
+                        error: null,
+                    } 
+            
+            default :
+            return state
+        }
+};
