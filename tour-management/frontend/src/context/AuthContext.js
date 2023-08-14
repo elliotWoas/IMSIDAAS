a2bbe7934889cb73d24a16171adaabb530002ae9
elliot sad <bamdadmahdi05@gmail.com>
2023-08-14 23:58:43 +0200

    Auth Context : localStorage , get item user

--- tour-management/frontend/src/context/AuthContext.js
+++ tour-management/frontend/src/context/AuthContext.js
@@ -1,65 +1,71 @@
-import { createContext, useEffect, useReducer } from "react"
+import { createContext, useEffect, useReducer } from "react";
 
 const initial_state = {
-    user: null,
-    loading: false,
-    error: null,
+  user:
+    localStorage.getItem("user") !== undefined
+      ? JSON.parse(localStorage.getItem("user"))
+      : null,
+  loading: false,
+  error: null,
 };
 
 export const AuthContext = createContext(initial_state);
 
 const AuthReducer = (state, action) => {
-    switch (action.type) {
-        case "LOGIN_START": 
-        return {
-            user: null,
-            loading: false,
-            error: null,
-        };
-        case 'LOGIN_SUCCESS' :
-            return {
-                user: action.playload,
-                loading: false,
-                error: null,
-            } 
-            case 'LOGIN_FAILURE' :
-                return {
-                    user: null,
-                    loading: false,
-                    error: action.playload,
-                } 
-                case 'REGISTER_SUCCESS' :
-                    return {
-                        user: null,
-                        loading: false,
-                        error: null,
-                    }
-                    case 'LOGOUT' :
-                    return {
-                        user: null,
-                        loading: false,
-                        error: null,
-                    } 
-            
-            default :
-            return state
-        }
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.playload,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.playload,
+      };
+    case "REGISTER_SUCCESS":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
 };
 
-export const AuthContextProvider = ({children})=> {
-    
-    const [state, dispatch] = useReducer(AuthReducer, initial_state)
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, initial_state);
 
-    useEffect(()=> {
-        localStorage.setItem('user', JSON.stringify(state.user))
-    },[state.user])
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
 
-    return <AuthContext.Provider value={{
-        user:state.user,
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
         loading: state.loading,
         error: state.error,
-        dispatch
-    }}>
-        {children}
+        dispatch,
+      }}
+    >
+      {children}
     </AuthContext.Provider>
-}
+  );
+};
