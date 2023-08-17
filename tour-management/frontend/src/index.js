8865433101d581888e9ffc31a0d6bf7e11d43fd8
elliot sad <bamdadmahdi05@gmail.com>
2023-08-17 23:58:51 +0200

    ..

--- tour-management/frontend/src/index.js
+++ tour-management/frontend/src/index.js
@@ -9,6 +9,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
