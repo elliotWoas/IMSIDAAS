dd467f2107e9111c77b074c03477eac482f884e3
elliot sad <bamdadmahdi05@gmail.com>
2023-09-14 23:55:28 +0200

    use fetch : comment set loading

--- tour-management/frontend/src/hooks/useFetch.js
+++ tour-management/frontend/src/hooks/useFetch.js
@@ -4,6 +4,8 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  // set loading 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
