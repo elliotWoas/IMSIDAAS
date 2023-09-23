0eb6f857a5049e796c15a87cae40b2c289c39481
elliot sad <bamdadmahdi05@gmail.com>
2023-09-24 01:34:50 +0200

    routers : fix error in navigate (s)

--- tour-management/frontend/src/router/Routers.jsx
+++ tour-management/frontend/src/router/Routers.jsx
@@ -15,7 +15,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/tours" element={<Tours />} />
-      <Route path="/tour/:id" element={<TourDetails />} />
+      <Route path="/tours/:id" element={<TourDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/thank-you" element={<ThankYou />} />
