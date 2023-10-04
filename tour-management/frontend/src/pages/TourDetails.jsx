a73841e1d76ace4f328ff1d556b37f84e015d7e0
elliot sad <bamdadmahdi05@gmail.com>
2023-10-04 23:09:48 +0200

    tourdetails : fixet error in import booking

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
 import calculateAvgRating from "../utils/avgRating";
 import avatar from "../assets/images/avatar.jpg";
-import Booking from "../components/booking/Booking";
+import Booking from "../components/Booking/Booking";
 import Newsletter from "../shared/Newsletter";
 import useFetch from "./../hooks/useFetch";
 import { BASE_URL } from "./../utils/config";
