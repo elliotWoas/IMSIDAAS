72f5c727c44528f38d72c9379024180fdb84340b
elliot sad <bamdadmahdi05@gmail.com>
2023-10-18 21:47:06 +0200

    header : add toggle menu

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -26,6 +26,7 @@ const nav__links = [
 const Header = () => {
   //sticky Header Func
   const headerRef = useRef(null);
+  const menuRef = useRef(null);
   const navigate = useNavigate();
   const { user, dispatch } = useContext(AuthContext);
 
@@ -53,6 +54,8 @@ const Header = () => {
     return window.removeEventListener("scroll", stickyHeaderFunc);
   });
 
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -65,7 +68,7 @@ const Header = () => {
             {/* ========= logo end========= */}
 
             {/* ========= menu start ========= */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-item-center gap-5">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -103,7 +106,7 @@ const Header = () => {
                   </>
                 )}
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i class="ri-menu-line"></i>
               </span>
             </div>
