c0463e2948e9bfd34cb0420b053545ba450a8c2d
elliot sad <bamdadmahdi05@gmail.com>
2024-02-04 23:35:57 +0100

    verify token : change code littile bet

--- tour-management/backend/utils/verifyToken.js
+++ tour-management/backend/utils/verifyToken.js
@@ -12,38 +12,40 @@ const verifyToken = (req, res, next) => {
     });
   }
 
-  //if token is exist, then verify the token
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) {
-      return res
-        .status(401)
-        .json({ success: false, message: "token is invalid" });
-    }
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = user;
     next();
-  });
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Token is invalid",
+    });
+  }
 };
 
 export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.role === "admin") {
       next();
     } else {
-      return res
-        .status(401)
-        .json({ success: false, message: "You're not authorized" });
+      return res.status(401).json({
+        success: false,
+        message: "You're not authorized",
+      });
     }
   });
 };
 
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  verifyToken(req, res, () => {
     if (req.user.role === "admin") {
       next();
     } else {
-      return res
-        .status(401)
-        .json({ success: false, message: "You're not authorize" });
+      return res.status(401).json({
+        success: false,
+        message: "You're not authorized",
+      });
     }
   });
 };
