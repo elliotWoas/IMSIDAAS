7454ab094eff889ef05d938920d8b24d2ac88ef4
elliot-sad <bamdadmahdi05@gmail.com>
2024-02-04 16:23:34 +0100

    optimized testimoials code

--- tour-management/frontend/src/components/Testimoial/Testimoials.jsx
+++ tour-management/frontend/src/components/Testimoial/Testimoials.jsx
@@ -4,16 +4,50 @@ import ava01 from "../../assets/images/ava-1.jpg";
 import ava02 from "../../assets/images/ava-2.jpg";
 import ava03 from "../../assets/images/ava-3.jpg";
 
-const Testimoials = () => {
+const testimonialsData = [
+  {
+    id: 1,
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi voluptas eaque dicta, accusantium exercitationem harum enim quos fuga inventore et culpa!",
+    name: "John Doe",
+    role: "Customer",
+    image: ava01,
+  },
+  {
+    id: 2,
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi voluptas eaque dicta, accusantium exercitationem harum enim quos fuga inventore et culpa!",
+    name: "Lia Franklin",
+    role: "Customer",
+    image: ava02,
+  },
+  {
+    id: 3,
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi voluptas eaque dicta, accusantium exercitationem harum enim quos fuga inventore et culpa!",
+    name: "John Doe",
+    role: "Customer",
+    image: ava03,
+  },
+  {
+    id: 4,
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi voluptas eaque dicta, accusantium exercitationem harum enim quos fuga inventore et culpa!",
+    name: "Lia Franklin",
+    role: "Customer",
+    image: ava02,
+  },
+];
+
+const Testimonials = () => {
   const settings = {
-    dost: true,
+    dots: true,
     infinite: true,
     autoplay: true,
     speed: 1000,
     swipeToSlide: true,
     autoplaySpeed: 2000,
     slidesToShow: 3,
-
     responsive: [
       {
         breakpoint: 992,
@@ -36,71 +70,24 @@ const Testimoials = () => {
 
   return (
     <Slider {...settings}>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi
-          voluptas eaque dicta, accusantium exercitationem harum enim quos fuga
-          inventore et culpa!
-        </p>
-
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava01} className="w-25 h-25 rounded-8" alt="" />
-          <div>
-            <h6 className="mb-0 mt-4">John Doe</h6>
-            <p>Customer</p>
+      {testimonialsData.map((testimonial) => (
+        <div className="testimonial py-4 px-3" key={testimonial.id}>
+          <p>{testimonial.text}</p>
+          <div className="d-flex align-items-center gap-4 mt-3">
+            <img
+              src={testimonial.image}
+              className="w-25 h-25 rounded-8"
+              alt=""
+            />
+            <div>
+              <h6 className="mb-0 mt-4">{testimonial.name}</h6>
+              <p>{testimonial.role}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi
-          voluptas eaque dicta, accusantium exercitationem harum enim quos fuga
-          inventore et culpa!
-        </p>
-
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava02} className="w-25 h-25 rounded-8" alt="" />
-          <div>
-            <h6 className="mb-0 mt-4">Lia Franklin</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi
-          voluptas eaque dicta, accusantium exercitationem harum enim quos fuga
-          inventore et culpa!
-        </p>
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava03} className="w-25 h-25 rounded-8" alt="" />
-          <div>
-            <h6 className="mb-0 mt-4">John Doe</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
-      <div className="testimonial py-4 px-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-          doloribus nulla alias magnam molestiae voluptatem quo molestias, sequi
-          voluptas eaque dicta, accusantium exercitationem harum enim quos fuga
-          inventore et culpa!
-        </p>
-
-        <div className="d-flex align-items-center gap-4 mt-3">
-          <img src={ava02} className="w-25 h-25 rounded-8" alt="" />
-          <div>
-            <h6 className="mb-0 mt-4">Lia Franklin</h6>
-            <p>Customer</p>
-          </div>
-        </div>
-      </div>
+      ))}
     </Slider>
   );
 };
 
-export default Testimoials;
+export default Testimonials;
