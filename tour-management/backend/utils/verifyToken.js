61b0984ca95c787aa9fc5e68b22ca715a1d91fe8
elliot sad <bamdadmahdi05@gmail.com>
2024-02-10 18:12:50 +0100

    verifytoken : comment session and use cookie

--- tour-management/backend/utils/verifyToken.js
+++ tour-management/backend/utils/verifyToken.js
@@ -50,3 +50,58 @@ export const verifyAdmin = (req, res, next) => {
     }
   });
 };
+
+// import env from "dotenv";
+// import jwt from "jsonwebtoken";
+// // import session from "express-session";
+// // import createError from "http-errors";
+
+// env.config();
+// const JWT_SECRET_KEY = "himynameis1382eefuck";
+
+// // Configure express-session middleware
+// const sessionMiddleware = session({
+//   secret:  JWT_SECRET_KEY, 
+//   resave: false,
+//   saveUninitialized: true
+// });
+
+// const verifyToken = (req, res, next) => {
+//   // Check if session exists
+//   if (!req.session || !req.session.userToken) {
+//     return next(createError(401, "You're not authorized"));
+//   }
+
+//   try {
+//     const user = jwt.verify(req.session.userToken, JWT_SECRET_KEY);
+//     req.user = user;
+//     next();
+//   } catch (err) {
+//     return next(createError(401, "Token is invalid"));
+//   }
+// };
+
+// export const verifyUser = (req, res, next) => {
+//   sessionMiddleware(req, res, () => {
+//     verifyToken(req, res, () => {
+//       if (req.user.id === req.params.id || req.user.role === "admin") {
+//         next();
+//       } else {
+//         return next(createError(401, "You're not authorized"));
+//       }
+//     });
+//   });
+// };
+
+// export const verifyAdmin = (req, res, next) => {
+//   sessionMiddleware(req, res, () => {
+//     verifyToken(req, res, () => {
+//       if (req.user.role === "admin") {
+//         next();
+//       } else {
+//         return next(createError(401, "You're not authorized"));
+//       }
+//     });
+//   });
+// };
+
