154fc1a89304898f2e156cdeb17dae46ae9d7f78
elliot sad <bamdadmahdi05@gmail.com>
2024-02-11 22:51:47 +0100

    add validator to validate email

--- tour-management/backend/models/User.js
+++ tour-management/backend/models/User.js
@@ -1,4 +1,11 @@
 import mongoose from "mongoose";
+import { validator } from "validator";
+
+//another way to validate the email
+// var validateEmail = (email) => {
+//   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   return re.test(email)
+// }
 
 const userSchema = new mongoose.Schema(
   {
@@ -9,8 +16,15 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
+      trim: true,
+      lowercase: true,
       unique: true,
+      required: [true `Email address is required`],
+      validate: [ validator ,  'invalid email'],
+      // match: [
+      //   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      //   "Please fill a valid email address",
+      // ],
     },
     password: {
       type: String,
